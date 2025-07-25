67d9f6d06d57fb2c7d9a48aa73f0ed1dcdc07bca
winters27 <brandonwinters27@yahoo.com>
2025-07-24 20:04:57 -0700

    Disable webpack cache to fix Cloudflare Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.cache = false;
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
