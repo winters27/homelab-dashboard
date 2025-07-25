9d72c673a9cd306730f7096e5d9273fb80764515
winters27 <brandonwinters27@yahoo.com>
2025-07-24 20:29:15 -0700

    feat: Configure for Cloudflare Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.cache = false;
-    return config;
-  },
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
